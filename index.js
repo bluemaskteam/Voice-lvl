2cbf1beb6cc244beed4ec867913b84fde6f205f3
bluemaskteam <uhexfcmctd@gmail.com>
2025-03-10 22:37:44 +0100

    index.js

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
 const { Client, Intents } = require('discord.js-selfbot-v13');
-const { joinVoiceChannel } = require('@discordjs/voice'); // استيراد مكتبة الصوت
-
+const { joinVoiceChannel } = require('@discordjs/voice');
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES],
   checkUpdate: false,
 });
-
-const TOKEN = "MTMyNjI4MjM5ODI5OTU4NjYxMQ.G8GGhp.A1RGJvTpJc3g39ykcPvOB_y0wsec5Fx_18uwws"; // استبدل هذا بتوكن حسابك
+const TOKEN = "MTMyNjI4MjM5ODI5OTU4NjYxMQ.G8GGhp.A1RGJvTpJc3g39ykcPvOB_y0wsec5Fx_18uwws";
 const VOICE_CHANNEL_ID = "1312339948774359070";
-
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-
-  // ابحث عن الغرفة الصوتية باستخدام الـ ID
   const voiceChannel = client.channels.cache.get(VOICE_CHANNEL_ID);
-
-  if (voiceChannel && voiceChannel.type === 'GUILD_VOICE') { // التحقق من نوع القناة
+  if (voiceChannel && voiceChannel.type === 'GUILD_VOICE') { 
     try {
-      // انضم إلى الغرفة الصوتية
       const connection = joinVoiceChannel({
         channelId: voiceChannel.id,
         guildId: voiceChannel.guild.id,
         adapterCreator: voiceChannel.guild.voiceAdapterCreator,
-        selfDeaf: false, // لا تضع نفسك على وضع "صامت"
-        selfMute: false, // لا تضع نفسك على وضع "كتم"
+        selfDeaf: false, 
+        selfMute: false, 
       });
       console.log(`Joined voice channel: ${voiceChannel.name}`);
     } catch (error) {
@@ -33,5 +26,4 @@ client.on('ready', () => {
     console.log('Voice channel not found or is not a valid voice channel.');
   }
 });
-
 client.login(TOKEN);
